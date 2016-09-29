8df6cbe696618d03d644b5e750fa38aa0a1c8923
aleframari <alefra@mesoamericana.edu.gt>
2016-09-29 14:09:26 -0600

    nuevo proyecto

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Postear
+def post_list(request):
+    posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/post_list.html', {'posts': posts})
